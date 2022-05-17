b821dd1f67723fda0d72800734f392bc674ec5bd
bshepeliuk <bogdan__sh@ukr.net>
2022-05-17 15:35:56 +0300

    pull out hooks for getting items from DeviceListView

--- src/features/devices/pages/DevicesView.tsx
+++ src/features/devices/pages/DevicesView.tsx
@@ -7,9 +7,13 @@ import { useSearchParams } from 'react-router-dom';
 import ActiveFilterListView from '@features/filters/components/ActiveFilterList/ActiveFilterListView';
 import DeviceListView from '../components/DeviceListView';
 import { DeviceListContainer, Wrapper } from '../styles/deviceView.styled';
+import useGetMoreDevices from '../hooks/useGetMoreDevices';
+import useGetDevices from '../hooks/useGetDevices';
 
 function DevicesView() {
   const containerRef = useRef<any>(null);
+  const { items, isLoading } = useGetDevices();
+  const { isLoadingMore, fetchMore } = useGetMoreDevices();
   const [searchParams] = useSearchParams();
 
   const activeFilters = getActiveSearchParamsEntries(searchParams);
@@ -25,7 +29,13 @@ function DevicesView() {
         <FilterSideBarView />
 
         <DeviceListContainer ref={containerRef}>
-          <DeviceListView containerRef={containerRef} />
+          <DeviceListView
+            containerRef={containerRef}
+            items={items}
+            isLoading={isLoading}
+            isLoadingMore={isLoadingMore}
+            fetchMore={fetchMore}
+          />
         </DeviceListContainer>
       </Wrapper>
     </>
