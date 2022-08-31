82df9fd225b0e051ea1e52e6a60c8c12ad3a352d
bshepeliuk <bogdan__sh@ukr.net>
2022-08-31 15:57:50 +0300

    ad circleci

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,13 @@
 module.exports = {
+  collectCoverage: true,
+  coverageReporters: ['text', 'cobertura'],
   moduleNameMapper: {
     '@src/(.*)$': '<rootDir>/src/$1',
     '@common/(.*)$': '<rootDir>/src/common/$1',
     '@features/(.*)$': '<rootDir>/src/features/$1',
-    '.+\\.(css|style|less|sass|scss|png|jpg|ttf|woff|woff2)$':
-      'jest-transform-stub',
+    '.+\\.(css|style|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
   },
-  coveragePathIgnorePatterns: [
-    'node_modules',
-    '<rootDir>/tests/mocks/',
-    '.styled.ts',
-  ],
+  coveragePathIgnorePatterns: ['node_modules', '<rootDir>/tests/mocks/', '.styled.ts'],
   // silent: true,
   setupFilesAfterEnv: ['<rootDir>/jest-setup.js'],
   testEnvironment: 'jsdom',
