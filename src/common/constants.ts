dac8636ccfdc6bab1d9ae3d8180cadd1dc74ccc2
bshepeliuk <bogdan__sh@ukr.net>
2022-09-04 20:19:59 +0300

    update server url

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,5 +1,8 @@
 export const isDev = process.env.NODE_ENV === 'development';
-export const BASE_API_URL = 'http://localhost:3000/api'; // https://marketplace-check-api.herokuapp.com/
+
+export const BASE_API_URL = isDev
+  ? 'http://localhost:3000/api'
+  : 'https://marketplace-api-production.up.railway.app/api';
 
 export const ROLES = {
   BUYER: 'BUYER',
