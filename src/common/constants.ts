3b232c5d047ed00069a645141b314b655b0afad5
bshepeliuk <bogdan__sh@ukr.net>
2023-02-19 16:40:35 +0200

    update BASE_URL

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,8 +1,6 @@
 export const isDev = process.env.NODE_ENV === 'development';
 
-export const BASE_API_URL = isDev
-  ? 'http://localhost:3000/api'
-  : 'https://marketplace-api-production.up.railway.app/api';
+export const BASE_API_URL = isDev ? 'http://localhost:3000/api' : 'https://marketplace-api.fly.dev/api';
 
 export const ROLES = {
   BUYER: 'BUYER',
